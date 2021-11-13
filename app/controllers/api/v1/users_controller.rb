d7f2c8b062af88c4d1066794d814a9ebada22723
vlezya <vlezyashop@gmail.com>
2021-11-13 16:26:21 +0200

    Adds destroy action to the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update]
+  before_action :set_user, only: [:show, :update, :destroy]
   
   #GET /users/:id
   def show
@@ -22,6 +22,11 @@ class Api::V1::UsersController < ApplicationController
     else
       render json: @user.errors, status: :unprocessable_entity
     end
+  end
+  
+  def destroy
+    @user.destroy
+    head 204
   end
   
   private
