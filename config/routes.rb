b46be650dc6a44dcd064d2e5492c2eb15c3c943c
vlezya <vlezyashop@gmail.com>
2021-11-13 14:41:38 +0200

    Set the versioning namespaces for API

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json } do
-    
+  namespace :api, defaults: { format: :json §} do
+    namespace :v1 do
+      
+    end
   end
 end
