f5008444ba2238b2f4b7a78849fc70afa968e24b
vlezya <vlezyashop@gmail.com>
2021-11-19 20:00:03 +0200

    Adds authenticable module for managing authentication methods

--- test/controllers/concerns/authenticable_test.rb
+++ test/controllers/concerns/authenticable_test.rb
@@ -1,3 +1,13 @@
+class MockController
+  include Authenticable
+  attr_accessor :request
+  
+  def initialize
+    mock_request = Struct.new(:headers)
+    self.request = mock_request.new({})
+  end
+end
+
 class AuthenticableTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
