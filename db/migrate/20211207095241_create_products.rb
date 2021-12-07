6027b5c316f0a99bcccd5d3d3a6868c71f4b64cc
vlezya <vlezyashop@gmail.com>
2021-12-07 12:09:09 +0200

    Adds some validations to products

--- db/migrate/20211207095241_create_products.rb
+++ db/migrate/20211207095241_create_products.rb
@@ -3,7 +3,8 @@ class CreateProducts < ActiveRecord::Migration[6.1]
     create_table :products do |t|
       t.string :title
       t.decimal :price
-
+      t.boolean :published
+      t.belongs_to :user, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,11 @@ ActiveRecord::Schema.define(version: 2021_12_07_095241) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.decimal "price"
+    t.boolean "published"
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,4 +30,5 @@ ActiveRecord::Schema.define(version: 2021_12_07_095241) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "products", "users"
 end
