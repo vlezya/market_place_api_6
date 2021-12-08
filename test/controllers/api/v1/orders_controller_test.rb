89b91909d05aac69642359257df07da6b5c813e0
vlezya <vlezyashop@gmail.com>
2021-12-08 15:59:13 +0200

    Allows the order to be placed along with product quantity

--- test/controllers/api/v1/orders_controller_test.rb
+++ test/controllers/api/v1/orders_controller_test.rb
@@ -3,9 +3,10 @@ require "test_helper"
 class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
   
   setup do
-    @order = orders(:one)
-    @order_params = { order: {
-    product_ids: [products(:one).id, products(:two).id], total: 50}}
+    @order = products(:one)
+    @order_params = {order: {product_ids_and_quantities: [
+      { product_id: products(:one).id, quantity: 2 },
+      { product_id: products(:two).id, quantity: 3 }, ]}}
   end
   
   test 'should forbid orders for unlogged' do
@@ -25,8 +26,8 @@ class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
   end
   
   test 'should show order' do
-    get api_v1_orders_url(@order), headers: { Authorization: JsonWebToken.encode(
-      user_id: @order.user_id) }, as: :json
+    get api_v1_orders_url(@order), headers: {
+      Authorization: JsonWebToken.encode(user_id: @order.user_id) }, as: :json
       
     assert_response :success
     
@@ -47,6 +48,17 @@ class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
       post api_v1_orders_url,
       params: @order_params, headers: { Authorization: JsonWebToken.encode(user_id:
       @order.user_id) }, as: :json
+    end
+    assert_response :created
+  end
+  
+  
+  test 'should create order with two products and placements' do
+    assert_difference('Order.count', 1) do
+      assert_difference('Placement.count', 2) do
+        post api_v1_orders_url, params: @order_params, as: :json,
+        headers: { Authorization: JsonWebToken.encode(user_id: @order.user_id) }
+      end
     end
     assert_response :created
   end
